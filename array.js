741fef6e68e8ddb95220e19c0d8d8418ec7c4ff3
Chainarate <jn.chainarate@gmail.com>
2023-05-22 16:24:08 +0700

    Create JavaScript array file

---
+++ array.js
@@ -0,0 +1,13 @@
+const myFavFood = ["Noodle", "Ramen", "Fish", "Corndog"]
+
+console.log(myFavFood.indexOf("Corndog"))
+
+myFavFood.push("Bingzoo", "Apple")
+myFavFood.push("Toast")
+myFavFood.unshift("Pineapple")
+
+console.log(myFavFood.slice(2,5))
+
+console.log(myFavFood)
+
+console.log(myFavFood.includes("Corndog"))
---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello World")
+console.log("Geno")
+
+function grade(score) {
+    if (score < 0 || score > 100) {
+        console.log("Please input socre between 0-100")
+        return
+    } 
+
+    score = Math.round(score)
+
+    if (score >= 80) {
+        console.log("You got A")
+    } else if (score >= 70) {
+        console.log("You got B")
+    } else if (score >= 60) {
+        console.log("You got C")
+    } else if (score >= 50) {
+        console.log("You got D")
+    } else {
+        console.log("You got F")
+    }
+}
+
+grade(69.5)
